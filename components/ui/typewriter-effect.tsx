157d62eb29dc09adb16cd7d537d95c25ff5bb0f9
Nabil El Maaroufi <93434382+nabilelmaa@users.noreply.github.com>
2024-07-22 19:27:27 +0100

    first commit

--- components/ui/typewriter-effect.tsx
+++ components/ui/typewriter-effect.tsx
@@ -72,7 +72,7 @@ export const TypewriterEffect = ({
   return (
     <div
       className={cn(
-        "text-base sm:text-md md:text-xl lg:text-3xl font-semibold text-center",
+        "text-2xl md:text-3xl lg:text-4xl font-bold text-center",
         className
       )}
     >
@@ -110,7 +110,7 @@ export const TypewriterEffectSmooth = ({
   className?: string;
   cursorClassName?: string;
 }) => {
-
+  // split text inside of words into array of characters
   const wordsArray = words.map((word) => {
     return {
       ...word,
@@ -156,7 +156,7 @@ export const TypewriterEffectSmooth = ({
         }}
       >
         <div
-          className="text-md sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold"
+          className="text-xs sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold"
           style={{
             whiteSpace: "nowrap",
           }}
